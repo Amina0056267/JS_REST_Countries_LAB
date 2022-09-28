e2ff1bf4a13bb5accbd8bb5ec250df670309fa06
Amina Salah <aminasalah56@outlook.com>
2022-09-28 19:05:01 +0100

    MVP completed

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 console.log("HI!")
 
-document.addEventListener("CountriesById", () => {
-    const CountriesById = document.getElementById("countriesById");
+document.addEventListener("DOMContentLoaded", () => {
+    const list = document.getElementById("#countries");
 
+
+//Fetch all countries from the above url, and save the results in a variable
+    const fetchAllCountries = async() => {
+        const response = await fetch("https://restcountries.com/v3.1/all");
+        const jsonData = await response.json();
+        const countryNames = []
+    
+// Generate li elements for each country object and add the name of each country to the element's textContent value.       
+        jsonData.forEach((country) => countryNames.push(country.name))
+    
+        const countryName = countryNames.map(name_array);
+    
+// Append the li elements to the ul element to display a list of country names
+
+        for(let i = 0; i < 200; i++){
+            const getCountryList = document.createElement("list")
+            getCountryList.textContent = countryName[i]
+            list.appendChild(getCountryList);
+
+        }
+
+
+    }
 })
-
-const fetchAllCountries = async() => {
-    const response = await fetch("https://restcountries.com/v3.1/all");
-    const jsonData = await response.json();
-    countriesById.id = jsonData.url;
-}
-
-const getCountryList
