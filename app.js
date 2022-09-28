f2c92fde6a600e73865f816828d82309dc25818c
Amina Salah <aminasalah56@outlook.com>
2022-09-28 15:57:26 +0100

    generating list elements

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 console.log("HI!")
 
+document.addEventListener("CountriesById", () => {
+    const CountriesById = document.getElementById("countriesById");
+
+})
+
+const fetchAllCountries = async() => {
+    const response = await fetch("https://restcountries.com/v3.1/all");
+    const jsonData = await response.json();
+    countriesById.id = jsonData.url;
+}
+
+const getCountryList
