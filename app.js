46e8aa45cde4eacae03ddce11409b0797a4b6aad
Amina Salah <aminasalah56@outlook.com>
2022-09-29 10:05:34 +0100

    applied solutions

--- app.js
+++ app.js
@@ -1,29 +1,36 @@
-console.log("HI!")
 
 document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOM loaded!"); // Always console everytime you use eventlistener etc.
+
     const list = document.getElementById("#countries");
 
 
-//Fetch all countries from the above url, and save the results in a variable
+//arrayOfCountries[0].name.common === "Angola"
+
+
     const fetchAllCountries = async() => {
+        const countryUl = document.querySelector("ul");
         const response = await fetch("https://restcountries.com/v3.1/all");
         const jsonData = await response.json();
         const countryNames = []
-    
-// Generate li elements for each country object and add the name of each country to the element's textContent value.       
+
+        const countryFlag = document.createElement("img");
+            countryFlag.src = country.flags.png;
+            countryFlag.classList.add("flag");
+            countryUl.appendChild(countryFlag);
+
+        }
+
         jsonData.forEach((country) => countryNames.push(country.name))
     
         const countryName = countryNames.map(name_array);
     
-// Append the li elements to the ul element to display a list of country names
-
-        for(let i = 0; i < 200; i++){
+        for(let i = 0; i < 250; i++){
             const getCountryList = document.createElement("list")
             getCountryList.textContent = countryName[i]
             list.appendChild(getCountryList);
-
         }
-
-
-    }
-})
+        
+        const button = document.querySelector("button");
+        button.addEventListener("click", fetchAllCountries);
+    });
